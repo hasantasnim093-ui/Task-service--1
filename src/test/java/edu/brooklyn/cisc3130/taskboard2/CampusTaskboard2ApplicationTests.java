afdba5c558ede839a7dd7f08ec0efce3c16b553a
Tasnim Hasan <hasantasnim093@gmail.com>
2026-05-08 13:29:02 -0400

    new files , security systems added

---
+++ src/test/java/edu/brooklyn/cisc3130/taskboard2/CampusTaskboard2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.brooklyn.cisc3130.taskboard2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusTaskboard2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
